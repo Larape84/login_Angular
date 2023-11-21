ec9be850131786d139fa1eb4d2bdb04b7a6ba6cd
jhonmario63 <jmsalazar1995@gmail.com>
2023-11-21 14:48:15 -0500

    se cambia diseño

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,9 +2,9 @@
 
 export const environment = {
   // baseUrl: 'https://nest-auth-backend.up.railway.app'
-  // baseUrl: 'http://localhost:8080/api'
+   baseUrl: 'http://localhost:8080/api'
   // baseUrl: 'https://5242-181-57-229-82.ngrok-free.app'
-  baseUrl: 'http://192.168.130.103:8080/api'
+  //baseUrl: 'http://192.168.130.103:8080/api'
 
 
 }
